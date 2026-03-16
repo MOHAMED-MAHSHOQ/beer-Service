6f809711ae74d8828e61512ce55b95f09e0f3b8d
MOHAMED-MAHSHOQ <msmahshoq2005@gmail.com>
2026-03-16 17:34:06 +0530

    Customer Assignment

--- src/main/java/com/code/lambok/bootstrap/BootstrapData.java
+++ src/main/java/com/code/lambok/bootstrap/BootstrapData.java
@@ -9,15 +9,18 @@ import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.Arrays;
 
 @Component
 @RequiredArgsConstructor
 public class BootstrapData implements CommandLineRunner {
     private final BeerRepository beerRepository;
+    private final CustomerRepository customerRepository;
 
     @Override
     public void run(String... args) throws Exception {
         loadBeerData();
+        loadCustomerData();
     }
 
     private void loadBeerData() {
@@ -58,4 +61,35 @@ public class BootstrapData implements CommandLineRunner {
         }
 
     }
+
+    private void loadCustomerData() {
+
+        if (customerRepository.count() == 0) {
+            Customer customer1 = Customer.builder()
+                    .name("Customer 1")
+                    .version(1)
+                    .createdDate(LocalDateTime.now())
+                    .updateDate(LocalDateTime.now())
+                    .build();
+
+            Customer customer2 = Customer.builder()
+                    .name("Customer 2")
+                    .version(1)
+                    .createdDate(LocalDateTime.now())
+                    .updateDate(LocalDateTime.now())
+                    .build();
+
+            Customer customer3 = Customer.builder()
+                    .name("Customer 3")
+                    .version(1)
+                    .createdDate(LocalDateTime.now())
+                    .updateDate(LocalDateTime.now())
+                    .build();
+
+            customerRepository.saveAll(Arrays.asList(customer1, customer2, customer3));
+        }
+
     }
+
+
+}
